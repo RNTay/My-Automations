14e7c6ddd56bdf47f5462ba9f190fe9a3416d342
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-10 17:29:17 +0800

    Create send-email.py

---
+++ send-email.py
@@ -0,0 +1,38 @@
+from email.message import EmailMessage
+import smtplib
+import getpass
+
+message = EmailMessage()
+
+sender = 'me@example.com'
+recipient = 'you@example.com'
+
+message['From'] = sender
+message['To'] = recipient
+
+message['Subject'] = 'email subject'
+
+body = '''email body
+bla bla
+text
+bla
+Regards,
+signature.
+'''
+message.set_content(body)
+
+print(message)  # print the message
+
+mail_server = smtplib.SMTP_SSL('smtp.gmail.com')  # smtp server address
+
+# If you want to see the SMTP messages that are
+# being sent back and forth by the smtplib module behind the scenes,
+# you can set the debug level on the SMTP or SMTP_SSL object.
+mail_server.set_debuglevel(1)
+
+mail_pass = getpass.getpass('Password? ')  # sender's email password
+
+mail_server.login(sender, mail_pass)
+mail_server.send_message(message)
+
+mail_server.quit()
