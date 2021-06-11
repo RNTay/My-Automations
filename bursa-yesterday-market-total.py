8a9256c7649dec8333de8a77a457bb7d3891b2f4
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-12 00:13:25 +0800

    added docstrings for functions

--- bursa-yesterday-market-total.py
+++ bursa-yesterday-market-total.py
@@ -8,6 +8,11 @@ import pdfminer.pdfparser
 
 
 def download_file(url: str) -> str:
+    """
+    Downloads the PDF file from the URL give.
+    :param url: URL to the PDF file
+    :return: name of the file downloaded
+    """
     local_filename = url.split('/')[-1]
 
     with requests.get(url) as r:
@@ -17,7 +22,13 @@ def download_file(url: str) -> str:
     return local_filename
 
 
-def create_pdf_and_get_text(local_filename: str, date: str) -> str:
+def get_text_from_pdf(local_filename: str, date: str) -> str:
+    """
+    Extracts the text from the page of the PDF which contains the grand total.
+    :param local_filename: name of the PDF file
+    :param date: the date in the name of the file
+    :return: the extracted text from the file
+    """
     try:
         with pdfplumber.open(local_filename) as pdf:
             page = pdf.pages[2]
@@ -32,7 +43,12 @@ def create_pdf_and_get_text(local_filename: str, date: str) -> str:
     return text
 
 
-def output_grand_total(data: str) -> tuple[str, str]:
+def relevant_grand_total(data: str) -> tuple[str, str]:
+    """
+    Gets the relevant grand total numbers, i.e. the volume and value.
+    :param data: the text which contains the grand total values
+    :return: grand total volume and value
+    """
     for row in data.splitlines():
         if row.startswith('Grand Total : Market Transaction'):
             numbers = row.split()[5:]
@@ -40,6 +56,13 @@ def output_grand_total(data: str) -> tuple[str, str]:
 
 
 def pretty_print(volume: str, value: str, date: str):
+    """
+    Outputs the grand total numbers.
+    :param volume: grand total volume
+    :param value: grand total value
+    :param date: date of the PDF file
+    :return: None
+    """
     print('\n' + '=' * 50 + '\n')
     print('Grand Total: Market Transaction\n')
     print('Date: ', date, '\n')
@@ -54,8 +77,8 @@ if __name__ == '__main__':
     securities_url = bursa_site + yesterday + '.pdf'
 
     securities = download_file(securities_url)
-    page_with_grand_total_text = create_pdf_and_get_text(securities, yesterday)
-    Vol, Val = output_grand_total(page_with_grand_total_text)
+    page_with_grand_total_text = get_text_from_pdf(securities, yesterday)
+    Vol, Val = relevant_grand_total(page_with_grand_total_text)
     pretty_print(Vol, Val, yesterday)
 
     os.remove(securities)
