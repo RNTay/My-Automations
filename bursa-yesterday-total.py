54d2b6bcff7c58456a7f4700db96468f322d1bfe
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-11 22:01:06 +0800

    Update bursa-yesterday-total.py

--- bursa-yesterday-total.py
+++ bursa-yesterday-total.py
@@ -2,8 +2,8 @@
 
 import requests
 import pdfplumber
-import datetime as dt
 import os
+import datetime as dt
 
 
 def download_file(url):
@@ -16,31 +16,44 @@ def download_file(url):
     return local_filename
 
 
-bursa_site = 'https://www.bursamalaysia.com/misc/missftp/securities/securities_equities_'
-yesterday = str(dt.datetime.now() - dt.timedelta(days=1))[:10]
-securities_url = bursa_site + yesterday + '.pdf'
+def create_pdf_and_get_text(local_filename, date):
+    try:
+        with pdfplumber.open(local_filename) as pdf:
+            page = pdf.pages[2]
+            text = page.extract_text()
+    except:
+        print('\n' + '=' * 50 + '\n')
+        print('Error: File does not exist for the date {}.'.format(date))
+        print('\n' + '=' * 50 + '\n')
+        os.remove(securities)
+        exit()
+    return text
 
-securities = download_file(securities_url)
 
-try:
-    with pdfplumber.open(securities) as pdf:
-        page = pdf.pages[2]
-        text = page.extract_text()
-except:
-    print('\n' + '='*50 + '\n')
-    print('Error: File does not exist for yesterday, {}.'.format(yesterday))
+def output_grand_total(data):
+    for row in data.splitlines():
+        if row.startswith('Grand Total : Market Transaction'):
+            numbers = row.split()[5:]
+            return numbers[0], numbers[-1]
+
+
+def pretty_print(volume, value, date):
     print('\n' + '=' * 50 + '\n')
+    print('Grand Total: Market Transaction\n')
+    print('Date: ', date, '\n')
+    print("Volume ('000 units):", volume)
+    print("Value (RM '000):", value)
+    print('\n' + '=' * 50 + '\n')
+
+
+if __name__ == '__main__':
+    bursa_site = 'https://www.bursamalaysia.com/misc/missftp/securities/securities_equities_'
+    yesterday = str(dt.datetime.now() - dt.timedelta(days=1))[:10]
+    securities_url = bursa_site + yesterday + '.pdf'
+
+    securities = download_file(securities_url)
+    page_with_grand_total_text = create_pdf_and_get_text(securities, yesterday)
+    Vol, Val = output_grand_total(page_with_grand_total_text)
+    pretty_print(Vol, Val, yesterday)
+
     os.remove(securities)
-    exit()
-
-for row in text.splitlines():
-    if row.startswith('Grand Total : Market Transaction'):
-        numbers = row.split()[5:]
-        print('\n' + '='*50 + '\n')
-        print('Grand Total: Market Transaction\n')
-        print('Date: ', yesterday, '\n')
-        print("Volume ('000 units):", numbers[0])
-        print("Value (RM '000):", numbers[-1])
-        print('\n' + '='*50 + '\n')
-
-os.remove(securities)
