edddf17528e544ad7bbc25ee52fe52794e07080f
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-11 20:55:25 +0800

    Create date_securities.py

---
+++ date_securities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import requests
+import pdfplumber
+import os
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+
+    with requests.get(url) as r:
+        with open(local_filename, 'wb') as f:
+            f.write(r.content)
+
+    return local_filename
+
+
+bursa_site = 'https://www.bursamalaysia.com/misc/missftp/securities/securities_equities_'
+yesterday = input('Date (YYYY-MM-DD): ')
+securities_url = bursa_site + yesterday + '.pdf'
+
+securities = download_file(securities_url)
+
+try:
+    with pdfplumber.open(securities) as pdf:
+        page = pdf.pages[2]
+        text = page.extract_text()
+except:
+    print('Error: File does not exist for the date, {}.'.format(yesterday))
+    os.remove(securities)
+    exit()
+
+for row in text.splitlines():
+    if row.startswith('Grand Total : Market Transaction'):
+        numbers = row.split()[5:]
+        print('Grand Total: Market Transaction')
+        print("Volume ('000 units):", numbers[0])
+        print("Value (RM '000):", numbers[-1])
+
+os.remove(securities)
