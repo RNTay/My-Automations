466ff07d6dce2e20ebfb16987073ca944203d709
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-11 21:59:52 +0800

    Update bursa-total.py

--- bursa-total.py
+++ bursa-total.py
@@ -15,14 +15,14 @@ def download_file(url):
     return local_filename
 
 
-def create_pdf_and_get_text(local_filename):
+def create_pdf_and_get_text(local_filename, date):
     try:
         with pdfplumber.open(local_filename) as pdf:
             page = pdf.pages[2]
             text = page.extract_text()
     except:
         print('\n' + '=' * 50 + '\n')
-        print('Error: File does not exist for the date {}.'.format(day))
+        print('Error: File does not exist for the date {}.'.format(date))
         print('\n' + '=' * 50 + '\n')
         os.remove(securities)
         exit()
@@ -36,10 +36,10 @@ def output_grand_total(data):
             return numbers[0], numbers[-1]
 
 
-def pretty_print(volume, value):
+def pretty_print(volume, value, date):
     print('\n' + '=' * 50 + '\n')
     print('Grand Total: Market Transaction\n')
-    print('Date: ', day, '\n')
+    print('Date: ', date, '\n')
     print("Volume ('000 units):", volume)
     print("Value (RM '000):", value)
     print('\n' + '=' * 50 + '\n')
@@ -52,8 +52,8 @@ if __name__ == '__main__':
     securities_url = bursa_site + day + '.pdf'
 
     securities = download_file(securities_url)
-    page_with_grand_total_text = create_pdf_and_get_text(securities)
+    page_with_grand_total_text = create_pdf_and_get_text(securities, day)
     Vol, Val = output_grand_total(page_with_grand_total_text)
-    pretty_print(Vol, Val)
+    pretty_print(Vol, Val, day)
 
     os.remove(securities)
