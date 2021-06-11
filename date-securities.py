10660bedd17c4d35acabcfce39162c96e25738aa
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-11 21:12:18 +0800

    Update date-securities.py

--- date-securities.py
+++ date-securities.py
@@ -16,8 +16,8 @@ def download_file(url):
 
 
 bursa_site = 'https://www.bursamalaysia.com/misc/missftp/securities/securities_equities_'
-yesterday = input('Date (YYYY-MM-DD): ')
-securities_url = bursa_site + yesterday + '.pdf'
+day = input('Date (YYYY-MM-DD): ')
+securities_url = bursa_site + day + '.pdf'
 
 securities = download_file(securities_url)
 
@@ -26,21 +26,20 @@ try:
         page = pdf.pages[2]
         text = page.extract_text()
 except:
-    print('Error: File does not exist for the date, {}.'.format(yesterday))
+    print('\n' + '=' * 50 + '\n')
+    print('Error: File does not exist for the date {}.'.format(day))
+    print('\n' + '=' * 50 + '\n')
     os.remove(securities)
     exit()
 
 for row in text.splitlines():
     if row.startswith('Grand Total : Market Transaction'):
         numbers = row.split()[5:]
-        print()
-        print('='*50)
-        print()
-        print('Grand Total: Market Transaction')
+        print('\n' + '=' * 50 + '\n')
+        print('Grand Total: Market Transaction\n')
+        print('Date: ', day, '\n')
         print("Volume ('000 units):", numbers[0])
         print("Value (RM '000):", numbers[-1])
-        print()
-        print('='*50)
-        print()
+        print('\n' + '=' * 50 + '\n')
 
 os.remove(securities)
