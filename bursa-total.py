22b52884ccc44cdcf4d30218e5097064ba6fe13d
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-11 21:55:46 +0800

    reorganised code

--- bursa-total.py
+++ bursa-total.py
@@ -15,32 +15,45 @@ def download_file(url):
     return local_filename
 
 
-bursa_site = 'https://www.bursamalaysia.com/misc/missftp/securities/securities_equities_'
-print()
-day = input('Date (YYYY-MM-DD): ')
-securities_url = bursa_site + day + '.pdf'
+def create_pdf_and_get_text(local_filename):
+    try:
+        with pdfplumber.open(local_filename) as pdf:
+            page = pdf.pages[2]
+            text = page.extract_text()
+    except:
+        print('\n' + '=' * 50 + '\n')
+        print('Error: File does not exist for the date {}.'.format(day))
+        print('\n' + '=' * 50 + '\n')
+        os.remove(securities)
+        exit()
+    return text
 
-securities = download_file(securities_url)
 
-try:
-    with pdfplumber.open(securities) as pdf:
-        page = pdf.pages[2]
-        text = page.extract_text()
-except:
+def output_grand_total(data):
+    for row in data.splitlines():
+        if row.startswith('Grand Total : Market Transaction'):
+            numbers = row.split()[5:]
+            return numbers[0], numbers[-1]
+
+
+def pretty_print(volume, value):
     print('\n' + '=' * 50 + '\n')
-    print('Error: File does not exist for the date {}.'.format(day))
+    print('Grand Total: Market Transaction\n')
+    print('Date: ', day, '\n')
+    print("Volume ('000 units):", volume)
+    print("Value (RM '000):", value)
     print('\n' + '=' * 50 + '\n')
+
+
+if __name__ == '__main__':
+    bursa_site = 'https://www.bursamalaysia.com/misc/missftp/securities/securities_equities_'
+    print()
+    day = input('Date (YYYY-MM-DD): ')
+    securities_url = bursa_site + day + '.pdf'
+
+    securities = download_file(securities_url)
+    page_with_grand_total_text = create_pdf_and_get_text(securities)
+    Vol, Val = output_grand_total(page_with_grand_total_text)
+    pretty_print(Vol, Val)
+
     os.remove(securities)
-    exit()
-
-for row in text.splitlines():
-    if row.startswith('Grand Total : Market Transaction'):
-        numbers = row.split()[5:]
-        print('\n' + '=' * 50 + '\n')
-        print('Grand Total: Market Transaction\n')
-        print('Date: ', day, '\n')
-        print("Volume ('000 units):", numbers[0])
-        print("Value (RM '000):", numbers[-1])
-        print('\n' + '=' * 50 + '\n')
-
-os.remove(securities)
