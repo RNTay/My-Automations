82d0c662483fb5b2f17af779f57b7e89adfd07ff
RNTay <81459529+RNTay@users.noreply.github.com>
2021-08-14 23:09:49 +0800

    Create popcat.py

---
+++ popcat.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+
+def main():
+    driver = webdriver.Safari()
+    driver.get('https://popcat.click')
+    cat = driver.find_element_by_xpath('//*[@id="app"]/div')
+    
+    for _ in range(10):
+        cat.send_keys('a bunch of keys'*100)
+
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
+
+
